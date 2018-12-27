e14490a57c6159bb1668bece675a95a0daec51df
Brodey Newman <brodeynewman@gmail.com>
2018-12-27 17:28:30 -0500

    mkdir

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,7 +14,8 @@ function push {
 
 echoBanner "Initializing deploy"
 
-cat > ~/.aws/credentials << EOF1
+mkdir .aws
+cat > .aws/credentials << EOF1
 [default]
 aws_access_key_id = $AWS_ACCESS_KEY
 aws_secret_access_key = $AWS_SECRET_TOKEN
