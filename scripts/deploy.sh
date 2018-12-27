9c7f777400844571684452329c9dbed9f910c78d
Brodey Newman <brodeynewman@gmail.com>
2018-12-27 16:41:58 -0500

    deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+function echoBanner {
+  echo "
+  ----------------------------
+  $1
+  ----------------------------
+  "
+}
+
+function push {
+  aws s3 sync ${TRAVIS_BUILD_DIR}/dist s3://$S3_PROD_BUCKET
+}
+
+echoBanner();
+
+cat > ~/.aws/credentials << EOF1
+[default]
+aws_access_key_id = $AWS_ACCESS_KEY
+aws_secret_access_key = $AWS_SECRET_TOKEN
+EOF1
+
+push();
